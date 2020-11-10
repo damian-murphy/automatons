a01b72a71b8f3d4af5fa47c3afeeb8e8ba302cd8
Murf <73829483+damian-murphy@users.noreply.github.com>
2020-11-10 11:42:55 +0000

    Update cleanup.py
    Added argparse bits and look for config file. Need to add yaml config next.

--- cleanup.py
+++ cleanup.py
@@ -2,8 +2,51 @@
 #  Copyright (c) 2020 damian@orchestrating-automatons.eu
 # Furthering my Learning Python series, I now try to manage my backups
 #
-import os, time
+import os, time, argparse, yaml
 
+CONFIGFILE = "cleanup-config.yml"
+CONFPATH = "/usr/local/etc/"
+DEBUG = False
+
+def parse_cmdline():
+    # Parse any command line options. Currently, we only support one.
+    # Argparse seems nice.
+    parser = argparse.ArgumentParser(description="cleans up a given directory, removing files older than X days."
+                                                 " Settings can be changed in the yaml formatted config file."
+    parser.add_argument("-d","--debug", action="store_true", help="print out some extra debugging info")
+    args = parser.parse_args()
+    return args
+
+# Import the config file
+def load_config(configfile):
+    # Let's be nice and look for the config file in a few places.
+
+    if os.path.exists(CONFIGFILE):
+        confpath = os.getcwd()
+        if DEBUG:
+            print "Using config: " + confpath + CONFIGFILE
+    elif os.path.exists("../etc/" + CONFIGFILE):
+        confpath = "../etc/"
+        if DEBUG:
+            print "Using config: " + confpath + CONFIGFILE
+    elif os.path.exists(CONFPATH + CONFIGFILE):
+        confpath = CONFPATH
+        if DEBUG:
+            print "Using config: " + confpath + CONFIGFILE
+    else:
+        print "Q - Parameter error, " + configfile + \
+              " not found or readable in ../etc, /usr/local/etc or " + os.getcwd()
+        exit(2)
+
+    # Load the config file
+    try:
+        config = yaml.safe_load(file(confpath+configfile, 'r'))
+        return config
+    except (yaml.YAMLError, IOError) as e:
+        print "R Tape Loading Error, ", e
+
+
+def main():
 backups = "/backup.dir"
 today = time.time()
 count = 0
