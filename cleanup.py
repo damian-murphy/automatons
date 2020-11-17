b225621365087899d91b47acf7d9b3a01ca0fb58
Damian <damian@orchestrating-automatons.eu>
2020-11-17 15:42:26 +0000

    Got it working, fixed args, added debug, dry-run, added functionality for these, fixed bug where os.getcwd() didn't include a trailing slash.

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-#  Copyright (c) 2020 damian@orchestrating-automatons.eu
+# Copyright (c) 2020 damian@orchestrating-automatons.eu
 # Furthering my Learning Python series, I now try to manage my backups
+# Licensed under the GNU GPL v3
 #
 import argparse
 import os
@@ -15,18 +16,21 @@ DEBUG = False
 def parse_cmdline():
     # Parse any command line options. Currently, we only support one.
     # Argparse seems nice.
-    parser = argparse.ArgumentParser(description="cleans up a given directory, removing files older than X days. "
+    parser = argparse.ArgumentParser(description="cleans up a defined set of directories, removing files older than X "
+                                                 "days. "
                                                  "Settings can be changed in the cleanup-config.yml file.")
     parser.add_argument("-d", "--debug", action="store_true", help="print out some extra debugging info")
+    parser.add_argument("-n", "--dry-run", action="store_true", help="show what would be done, but don't do anything")
     args = parser.parse_args()
     return args
 
 
 def load_config(configfile):
     # Let's be nice and look for the config file in a few places.
+    global DEBUG
 
     if os.path.exists(CONFIGFILE):
-        confpath = os.getcwd()
+        confpath = os.getcwd() + "/"
         if DEBUG:
             print("Using config: " + confpath + CONFIGFILE)
     elif os.path.exists("../etc/" + CONFIGFILE):
@@ -45,27 +49,40 @@ def load_config(configfile):
     # Load the config file
     try:
         config = yaml.safe_load(open(confpath + configfile, 'r'))
+        # TODO: Put in checks here so that the config is confirmed or set to some defaults.
         return config
     except (yaml.YAMLError, IOError) as e:
         print("R Tape Loading Error, ", e)
 
 
 def main():
-    backups = "/backup.dir"
+    global DEBUG
+
     today = time.time()
     count = 0
-    retention_days = 7
 
-    for f in os.listdir(backups):
-        fpath = os.path.join(backups, f)
-        if os.stat(fpath).st_mtime < today - retention_days * 86400:
-            if os.path.isfile(fpath):
-                print("Found " + f + ", which is about " + str(round((today - os.stat(fpath).st_mtime) / 86400)) +
-                      " days old, deleting.")
-                os.remove(os.path.join(fpath))
-                count = count + 1
+    cli = parse_cmdline()
+    DEBUG = cli.debug
+    DRYRUN = cli.dry_run
+    config = load_config(CONFIGFILE)
 
-    print("Done - removed " + str(count) + " files, hopefully that's what was desired.")
+    if DEBUG:
+        print(config)
+
+    for item in config['directory_targets']:
+        # take a folder at a time, obvs.
+        for f in os.listdir(item):
+            fpath = os.path.join(item, f)
+            if os.stat(fpath).st_mtime < today - config['days'] * 86400:
+                if os.path.isfile(fpath):
+                    print("Found " + f + ", which is about " + str(round((today - os.stat(fpath).st_mtime) / 86400)) +
+                          " days old, deleting.")
+                    if not DRYRUN:
+                        os.remove(os.path.join(fpath))
+                    count = count + 1
+
+        print("Done - removed " + str(count) + " files from " + item + ", hopefully that's what was desired.")
+
     print("0 OK, 0:1")
 
 
